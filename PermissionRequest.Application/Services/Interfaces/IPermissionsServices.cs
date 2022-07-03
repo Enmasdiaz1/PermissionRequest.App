be769880cd16c12f86067598908fd7a10269c40d
Enmasdiaz <enmasd02@gmail.com>
2022-07-02 21:19:45 -0400

    Capa Application Realizada

---
+++ PermissionRequest.Application/Services/Interfaces/IPermissionsServices.cs
@@ -0,0 +1,19 @@
+﻿using PermissionRequest.App.PermissionRequest.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermissionRequest.Application.Services.Interfaces
+{
+    public interface IPermissionsServices
+    {
+        IEnumerable<PermissionsModel> GetPermissions();
+        PermissionsModel GetPermission(int id);
+        void InsertPermission(PermissionsModel permissionsModel);
+        void UpdatePermission(PermissionsModel permissionsModel);
+        void DeletePermission(int id);  
+        
+    }
+}
