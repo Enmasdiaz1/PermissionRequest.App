56570afaadd2af7c77911499b851f21f5e82cf56
Enmasdiaz <enmasd02@gmail.com>
2022-07-02 20:25:59 -0400

    Mapeo de ambos models hecho

---
+++ Domain/PermissionsTypeMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PermissionRequest.App.PermissionRequest.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermissionRequest.Domain
+{
+    public  class PermissionsTypeMap
+    {
+        public PermissionsTypeMap(EntityTypeBuilder<PermissionsTypeModel>entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(x => x.Id);
+            entityTypeBuilder.Property(x => x.Descripcion).IsRequired();
+        }
+    }
+}
