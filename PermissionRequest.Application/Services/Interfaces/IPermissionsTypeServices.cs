f17a9ef83312c42f1ec9b9af9b46c2328213926e
Enmasdiaz <enmasd02@gmail.com>
2022-07-02 21:19:45 -0400

    Capa Application Realizada

---
+++ PermissionRequest.Application/Services/Interfaces/IPermissionsTypeServices.cs
@@ -0,0 +1,15 @@
+﻿using PermissionRequest.App.PermissionRequest.Domain.Models;
+using PermissionRequest.Application.Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermissionRequest.Application.Services.Interfaces
+{
+    public interface IPermissionsTypeServices
+    {
+        PermissionsTypeModel GetPermissions(int id);
+    }
+}
---
+++ PermissionRequest.Application/Services/Services/PermissionsServices.cs
@@ -0,0 +1,53 @@
+﻿using PermissionRequest.App.PermissionRequest.Domain.Models;
+using PermissionRequest.Application.Services.Interfaces;
+using PermissionRequest.Infraestructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermissionRequest.Application.Services.Services
+{
+    public class PermissionsServices:IPermissionsServices
+    {
+        private readonly IPermissionsRepository<PermissionsModel> permissionsRepository;
+        private readonly IPermissionsRepository<PermissionsTypeModel> permissionsTypeRepository;
+
+        public PermissionsServices( IPermissionsRepository<PermissionsModel> permissionsRepository,
+                                    IPermissionsRepository<PermissionsTypeModel>permissionsTypeRepository)
+        {
+            this.permissionsRepository = permissionsRepository;
+            this.permissionsTypeRepository = permissionsTypeRepository;
+        }
+
+
+        public void DeletePermission(int id)
+        {
+            PermissionsModel permissionsModel = this.permissionsRepository.Get(id);
+            permissionsRepository.Remove(permissionsModel);
+            PermissionsTypeModel permissionsTypeModel = this.permissionsTypeRepository.Get(id);
+            permissionsTypeRepository.Remove(permissionsTypeModel); 
+        }
+
+        public PermissionsModel GetPermission(int id)
+        {
+            return permissionsRepository.Get(id);
+        }
+
+        public IEnumerable<PermissionsModel> GetPermissions()
+        {
+            return permissionsRepository.GetAll();
+        }
+
+        public void InsertPermission(PermissionsModel permissionsModel)
+        {
+            permissionsRepository.Insert(permissionsModel);
+        }
+
+        public void UpdatePermission(PermissionsModel permissionsModel)
+        {
+            permissionsRepository.Update(permissionsModel);
+        }
+    }
+}
