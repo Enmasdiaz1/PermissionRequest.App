65cf3f7546a5c44079bbf697f02c3967f806f456
Enmasdiaz <enmasd02@gmail.com>
2022-07-02 20:21:20 -0400

    DbContext añadido, Domain e Infraestructura creadas.. sometidas a cambios o modificaciones

---
+++ Domain/Models/BaseClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermissionRequest.Domain.Models
+{
+    public  class BaseClass
+    {
+        public int Id { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string IpAddress { get; set; }
+    }
+}
---
+++ Domain/PermissionsMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PermissionRequest.App.PermissionRequest.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class PermissionsMap
+    {
+        public PermissionsMap(EntityTypeBuilder<PermissionsModel>entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(x => x.Id);    
+            entityTypeBuilder.Property(x=> x.NombreEmpleado).IsRequired();
+            entityTypeBuilder.Property(x=> x.ApellidoEmpleado).IsRequired();
+            entityTypeBuilder.Property(x=> x.TipoPermiso).IsRequired();
+            entityTypeBuilder.Property(x=> x.FechaPermiso).IsRequired();
+            entityTypeBuilder.HasOne(x => x.PermissionsType).WithOne(x => x.Permissions)
+                .HasForeignKey<PermissionsTypeModel>(x => x.Id);
+        }
+    }
+}
